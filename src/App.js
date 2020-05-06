358757d05ae831b1189cc799ddcae2fcb9fc4913
Vit0n <chumachenko.vic@gmail.com>
2020-05-06 17:40:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AppNavbar from './components/AppNavbar/AppNavbar';
+import Index from './components/AppPages/Index/Index';
+import ShoppingCartAside from './components/UI/ShoppingCartAside/ShoppingCartAside';
+import AppFooter from './containers/AppFooter/AppFooter';
+import SearchModal from './components/SearchModal/SearchModal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        isCardAsideShown: false,
+        isSearchModalShown: false,
+    };
+
+    componentDidMount() {
+        document.title = 'Haven Furniture';
+    }
+
+    closeCardAsideHandler = () => {
+        document.body.style.overflowY = 'scroll';
+        this.setState({
+            isCardAsideShown: false,
+        });
+    };
+
+    openCardAsideHandler = () => {
+        document.body.style.overflow = 'hidden';
+        this.setState({
+            isCardAsideShown: true,
+        });
+    };
+
+    closeSearchModalHandler = () => {
+        document.body.style.overflowY = 'scroll';
+        this.setState({
+            isSearchModalShown: false,
+        });
+    };
+
+    openSearchModalHandler = () => {
+        document.body.style.overflow = 'hidden';
+        this.setState({
+            isSearchModalShown: true,
+        });
+    };
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <AppNavbar
+                        openCardAside={this.openCardAsideHandler}
+                        openSearchModal={this.openSearchModalHandler}
+                    />
+                    <Switch>
+                        <Route path="/">
+                            <Index />
+                        </Route>
+                    </Switch>
+                    <AppFooter />
+
+                    {this.state.isCardAsideShown ? (
+                        <ShoppingCartAside closeCardAside={this.closeCardAsideHandler} />
+                    ) : null}
+
+                    {this.state.isSearchModalShown ? (
+                        <SearchModal closeSearchModal={this.closeSearchModalHandler} />
+                    ) : null}
+                </div>
+            </Router>
+        );
+    }
 }
-
-export default App;
